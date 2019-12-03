942274cd56be4d159ac344d85aee759d9fa03699
ecl5pse <fantamas20@gmail.com>
2019-12-03 20:57:20 +0200

    tryCatch

--- assets/view_model/createCalendarBodyElem.js
+++ assets/view_model/createCalendarBodyElem.js
@@ -1,12 +1,57 @@
 'use strict';
 
 
+
+function createWeekElem(week) {
+    const tableElementRow = document.createElement('tr');
+
+    week.days.forEach(
+        day=>{
+
+             tableElementRow.appendChild(createNumberDay(day));
+
+
+        }
+    );
+    return tableElementRow
+
+}
+
+
+
+function createNumberDay(day) {
+
+    const tableElement = document.createElement('td');
+
+    tableElement.innerText = day.date.toLocaleString("ru-Ru",{day:"2-digit"});
+
+    return tableElement;
+
+}
+
+
+
+
+
+
+
+
+
+
+
 export default function (month) {
+
+
     const tableBodyElem = document.createElement('tbody');
 
+    month.weeks.forEach(
+        week=>{
+
+            tableBodyElem.appendChild(createWeekElem(week) );
 
 
-
+        }
+    );
 
     return tableBodyElem;
 
---
+++ tryCatch/index.js
@@ -0,0 +1,51 @@
+'use strict';
+
+/*
+const userInput = prompt("from 1 to 10");
+
+
+const  userDigit = Number(userInput);
+
+
+
+
+
+
+try {
+    //
+    console.log("try");
+    d =10;
+
+}catch (e) {
+
+    console.log('gjfdkjgd')
+
+}*/
+
+
+//
+// function  foo (number) {
+//
+//
+//     if(isNaN(number)){
+//         throw new TypeError('param is not number');
+//     }
+//
+//
+//     console.log('magic');
+// }
+
+
+
+
+try {
+    console.log('Hello');
+   //throw new Error('')
+
+}catch (e) {
+
+    console.log('TUK');
+
+}finally {
+    console.log("Dead");
+}
